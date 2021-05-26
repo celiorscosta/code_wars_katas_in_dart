e5fafa4160580671f3831d4d88cabb02e3d7fde2
celiorscosta <celiorscosta@hotmail.com>
2021-05-26 13:27:35 -0300

    first example and the best answers

--- bin/katas/find_first_non-consecutive_number.dart
+++ bin/katas/find_first_non-consecutive_number.dart
@@ -1,4 +1,5 @@
 class CalculateNumber {
+  //My Solution
   int firstNonConsecutive(List<int> arr) {
     var index = arr?.first;
 
@@ -10,4 +11,32 @@ class CalculateNumber {
     }
     return null;
   }
+
+  //Recursive.
+  int firstNonConsecutive_recursive(arr) {
+    if (arr.length < 2) {
+      return null;
+    } else {
+      return arr[0] + 1 != arr[1]
+          ? arr[1]
+          : firstNonConsecutive(arr.sublist(1));
+    }
+  }
+
+  //Simple For
+  int firstNonConsecutive_simple(arr) {
+    for (var i = 0; i < arr.length - 1; i++) {
+      if (arr[i] + 1 != arr[i + 1]) {
+        return arr[i + 1];
+      }
+    }
+    return null;
+  }
+
+  int firstNonConsecutive_inLine(List<int> arr) => arr
+      .asMap()
+      .entries
+      .firstWhere((x) => x.key != 0 && arr[x.key - 1] != x.value - 1,
+          orElse: () => null)
+      ?.value;
 }
--- test/katas/find_first_non-consecutive_number_test.dart
+++ test/katas/find_first_non-consecutive_number_test.dart
@@ -4,8 +4,23 @@ import '../../bin/katas/find_first_non-consecutive_number.dart';
 void main() {
   var calculateNumber = CalculateNumber();
   test('a simple example', () {
-    final first =
+    var first =
         calculateNumber.firstNonConsecutive(const [0, 1, 2, 3, 4, 6, 7, 8]);
+
+    expect(first, equals(6));
+
+    first = calculateNumber
+        .firstNonConsecutive_recursive(const [0, 1, 2, 3, 4, 6, 7, 8]);
+
+    expect(first, equals(6));
+
+    first = calculateNumber
+        .firstNonConsecutive_simple(const [0, 1, 2, 3, 4, 6, 7, 8]);
+
+    expect(first, equals(6));
+
+    first = calculateNumber
+        .firstNonConsecutive_inLine(const [0, 1, 2, 3, 4, 6, 7, 8]);
 
     expect(first, equals(6));
   });
