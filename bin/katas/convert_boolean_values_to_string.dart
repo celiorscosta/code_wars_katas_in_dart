a6c8b43e4a1c451aa8cf07ca915f72d71c3c6c3c
celiorscosta <celiorscosta@hotmail.com>
2021-05-27 08:54:22 -0300

    Convert boolean values to strings 'Yes' or 'No'.

---
+++ bin/katas/convert_boolean_values_to_string.dart
@@ -0,0 +1,3 @@
+String bool_to_word(bool boolean) {
+  return boolean == true ? 'Yes' : 'No';
+}
---
+++ test/katas/convert_boolean_values_to_string_test.dart
@@ -0,0 +1,10 @@
+import 'package:test/test.dart';
+
+import '../../bin/katas/convert_boolean_values_to_string.dart';
+
+void main() {
+  test('Tests', () {
+    expect(bool_to_word(true), equals('Yes'));
+    expect(bool_to_word(false), equals('No'));
+  });
+}
