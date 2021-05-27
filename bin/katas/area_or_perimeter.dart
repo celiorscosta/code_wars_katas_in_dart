02827ded58d7c3afc8f564899fbf1be6c7c1f696
celiorscosta <celiorscosta@hotmail.com>
2021-05-27 09:05:30 -0300

    area or perimeter.

---
+++ bin/katas/area_or_perimeter.dart
@@ -0,0 +1,1 @@
+int area_or_perimeter(int l, int w) => l == w ? l * w : 2 * (l + w);
---
+++ test/katas/area_or_perimeter_test.dart
@@ -0,0 +1,11 @@
+import 'package:test/test.dart';
+import '../../bin/katas/area_or_perimeter.dart';
+
+void main() {
+  group('area_or_perimeter', () {
+    test('fixed tests', () {
+      expect(area_or_perimeter(4, 4), equals(16));
+      expect(area_or_perimeter(6, 10), equals(32));
+    });
+  });
+}
