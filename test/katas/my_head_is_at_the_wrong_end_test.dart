c9e6d2d0802855e0a36807fb6cce1f5a153c368b
celiorscosta <celiorscosta@hotmail.com>
2021-05-31 09:53:40 -0300

    My_head_is_at_the_wrong_end

---
+++ test/katas/my_head_is_at_the_wrong_end_test.dart
@@ -0,0 +1,16 @@
+import 'package:test/test.dart';
+import '../../bin/katas/my_head_is_at_the_wrong_end.dart';
+
+void testing(List<String> arr, List<String> exp) => test(
+    "Testing for ${arr.map((x) => "\"$x\"").toList()}",
+    () => expect(fixTheMeerkat(arr), equals(exp)));
+void main() {
+  group('Basic tests', () {
+    testing(['tail', 'body', 'head'], ['head', 'body', 'tail']);
+    testing(['tails', 'body', 'heads'], ['heads', 'body', 'tails']);
+    testing(['bottom', 'middle', 'top'], ['top', 'middle', 'bottom']);
+    testing(['lower legs', 'torso', 'upper legs'],
+        ['upper legs', 'torso', 'lower legs']);
+    testing(['ground', 'rainbow', 'sky'], ['sky', 'rainbow', 'ground']);
+  });
+}
