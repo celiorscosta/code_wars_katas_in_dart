12be90f32456e7e018fdc29db7e80667871a0744
celiorscosta <celiorscosta@hotmail.com>
2021-06-02 09:13:44 -0300

    refactoring

--- bin/katas/7kyu/flatten_and_sort_an_array.dart
+++ bin/katas/7kyu/flatten_and_sort_an_array.dart
@@ -1,6 +1,9 @@
-List<int> flattenAndSort(List<List<int>> nums) {
-  var newList = <int>[];
-  nums.forEach((element) => newList.addAll(element));
-  newList.sort();
-  return newList;
-}
+List<int> flattenAndSort(List<List<int>> nums) =>
+    nums.expand((p) => p).toList()..sort();
+    
+// List<int> flattenAndSort(List<List<int>> nums) {
+//   var newList = <int>[];
+//   nums.forEach((element) => newList.addAll(element));
+//   newList.sort();
+//   return newList;
+// }
