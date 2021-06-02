f9d53a019fcc8eb0b8ffb7c2a03077af95fcd198
celiorscosta <celiorscosta@hotmail.com>
2021-06-02 13:36:25 -0300

    refactory

--- bin/katas/7kyu/alphabet_simetry.dart
+++ bin/katas/7kyu/alphabet_simetry.dart
@@ -1,8 +1,7 @@
-List<int> solve(List<String> arr) {
+List<int> mySolution(List<String> arr) {
   var char_a = 'a'.codeUnitAt(0);
   var letters = String.fromCharCodes(Iterable.generate(26, (x) => char_a + x));
   var result = <int>[];
-
   arr.forEach((element) {
     var count = 0;
     for (var i = 0; i < element.length; i++) {
@@ -10,8 +9,19 @@ List<int> solve(List<String> arr) {
         count++;
       }
     }
-
     result.add(count);
   });
   return result;
+}
+
+List<int> solve(List<String> arr) {
+  return List<int>.from(arr.map((str) => str
+      .toLowerCase()
+      .codeUnits
+      .asMap()
+      .entries
+      .fold(
+          0,
+          (acc, pair) =>
+              acc + (pair.key == (pair.value - 'a'.codeUnitAt(0)) ? 1 : 0))));
 }
